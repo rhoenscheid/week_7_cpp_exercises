15dd5f781e122d9d63304d9d6d9a833b1a672556
Michael A. McLeod <michael.mcleod.13@ucl.ac.uk>
2023-03-03 12:25:53 +0000

    Improve summing alg types and loop

--- sum_examples/include/Summing.h
+++ sum_examples/include/Summing.h
@@ -4,15 +4,15 @@
 #include <vector>
 using std::vector;
 
-template<typename T>
-T Kahan_sum(const vector<T> &vals, const long N)
+template<typename FPtype>
+FPtype Kahan_sum(const vector<FPtype> &vals)
 {
-    T sum = 0;
-    T c = 0;
-    T y, t;
-    for (long i = 0; i < N; i++)
+    FPtype sum = 0;
+    FPtype c = 0;
+    FPtype y, t;
+    for (auto &x : vals)
     {
-        y = vals[i] - c;
+        y = x - c;
         t = sum + y;
         c = (t - sum) - y;
         sum = t;
@@ -22,12 +22,12 @@ T Kahan_sum(const vector<T> &vals, const long N)
 }
 
 template<typename T>
-T trivial_sum(const vector<T> &vals, const long N)
+T trivial_sum(const vector<T> &vals)
 {
     T sum = 0;
-    for(long i = 0; i < N; i++)
+    for(auto &x : vals)
     {
-        sum = sum + vals[i];
+        sum = sum + x;
     }
     return sum;
 }
