f9a8e82a09a757231dd412c5b283781a1685cd66
Milan Malfait <m.malfait@ucl.ac.uk>
2023-12-08 12:25:18 +0000

    Need to `#include <ostream>`

--- map_examples/SortedList.h
+++ map_examples/SortedList.h
@@ -1,100 +1,79 @@
+#include <ostream>
 #include <stdexcept>
 #include <vector>
+
 using std::vector;
 
-template<typename keyType, typename valType>
-class SortedList
-{
-    struct kv_pair
-    {
+template <typename keyType, typename valType>
+class SortedList {
+    struct kv_pair {
         keyType key;
         valType value;
     };
 
-    public:
-    SortedList(){}
+   public:
+    SortedList() {}
 
-    void insert(const keyType key, const valType value)
-    {
-        if(list.empty())
-        {
+    void insert(const keyType key, const valType value) {
+        if (list.empty()) {
             list.push_back(kv_pair{key, value});
-        }
-        else
-        {
-            for (size_t i = list.size() - 1; i > 0; i--)
-            {
-                if (key == list[i].key)
-                {
+        } else {
+            for (size_t i = list.size() - 1; i > 0; i--) {
+                if (key == list[i].key) {
                     list[i].value = value;
                     return;
-                }
-                else if (key > list[i].key)
-                {
-                    return insertAndShift(key, value, i+1);
+                } else if (key > list[i].key) {
+                    return insertAndShift(key, value, i + 1);
                 }
             }
             key > list[0].key ? insertAndShift(key, value, 1) : insertAndShift(key, value, 0);
         }
     }
 
-    valType lookup(keyType key)
-    {
-        if(list.empty()) throw std::runtime_error("Key not found.");
+    valType lookup(keyType key) {
+        if (list.empty()) throw std::runtime_error("Key not found.");
 
-        size_t i_max = list.size()-1;
+        size_t i_max = list.size() - 1;
         size_t i_min = 0;
-        size_t i = list.size()/2;
-        while(i_min != i_max)
-        {
-            if(key == list[i].key)
-            {
+        size_t i = list.size() / 2;
+        while (i_min != i_max) {
+            if (key == list[i].key) {
                 return list[i].value;
-            }
-            else if(key < list[i].key)
-            {
-                i_max = i-1;
-                i = i_min + (i_max - i_min)/2;
-            }
-            else
-            {
-                i_min = i+1;
-                i = i_min + (i_max - i_min)/2;
+            } else if (key < list[i].key) {
+                i_max = i - 1;
+                i = i_min + (i_max - i_min) / 2;
+            } else {
+                i_min = i + 1;
+                i = i_min + (i_max - i_min) / 2;
             }
         }
 
         // only one value left
-        if(key == list[i].key)
-        {
+        if (key == list[i].key) {
             return list[i].value;
-        }
-        else
-        {
+        } else {
             throw std::runtime_error("Key not found.");
         }
     }
 
-    friend std::ostream &operator<<(std::ostream &out, SortedList &L)
-    {
+    friend std::ostream &operator<<(std::ostream &out, SortedList &L) {
         out << "[";
-        for(const auto &x : L.list)
-        {
-            out << "(" << x.key << "," << x.value << ")" << " ";
+        for (const auto &x : L.list) {
+            out << "(" << x.key << "," << x.value << ")"
+                << " ";
         }
         out << "]";
         return out;
     }
 
-    private:
-    // This method can be replaced by std lib vector::insert with the same 
-    // complexity but we have written it out explicitly. 
-    void insertAndShift(const keyType key, const valType value, size_t i)
-    {
+   private:
+    // This method can be replaced by std lib vector::insert with the same
+    // complexity but we have written it out explicitly.
+    void insertAndShift(const keyType key, const valType value, size_t i) {
         kv_pair buffer_pair;
         kv_pair insertion_pair{key, value};
 
-        while(i < list.size())
-        {
+        while (i < list.size()) {
             buffer_pair = list[i];
             list[i] = insertion_pair;
             insertion_pair = buffer_pair;
--- map_examples/Tree.h
+++ map_examples/Tree.h
@@ -1,88 +1,60 @@
-#include <stdexcept>
 #include <memory>
+#include <ostream>
+#include <stdexcept>
 
-template<typename keyType, typename valType>
-class BinaryTree
-{
-    public:
-    BinaryTree(keyType k, valType v) : key(k), value(v)
-    {
-        parent = nullptr;
-    }
+template <typename keyType, typename valType>
+class BinaryTree {
+   public:
+    BinaryTree(keyType k, valType v) : key(k), value(v) { parent = nullptr; }
 
-    void insert(const keyType k, const valType v)
-    {
-        if(k == key)
-        {
+    void insert(const keyType k, const valType v) {
+        if (k == key) {
             value = v;
-        }
-        else if(k < key)
-        {
+        } else if (k < key) {
             insertChild(k, v, left);
-        }
-        else
-        {
+        } else {
             insertChild(k, v, right);
         }
     }
 
-    valType lookup(const keyType k)
-    {
-        if(k == key)
-        {
+    valType lookup(const keyType k) {
+        if (k == key) {
             return value;
-        }
-        else if(k < key)
-        {
+        } else if (k < key) {
             return lookupChild(k, left);
-        }
-        else
-        {
+        } else {
             return lookupChild(k, right);
         }
     }
 
-    friend std::ostream &operator<<(std::ostream &out, const BinaryTree &B)
-    {
+    friend std::ostream &operator<<(std::ostream &out, const BinaryTree &B) {
         out << "([" << B.key << "," << B.value << "], " << B.left << ", " << B.right << ")";
         return out;
     }
 
-   friend std::ostream &operator<<(std::ostream &out, const std::unique_ptr<BinaryTree> &Bptr)
-   {
-       if (Bptr)
-       {
-           out << (*Bptr);
-       }
-       else
-       {
-           out << "(Leaf)";
-       }
-       return out;
-   }
-
-    private:
-    void insertChild(const keyType k, const valType v, std::unique_ptr<BinaryTree> &c)
-    {
-        if(c)
-        {
-            c->insert(k, v);
+    friend std::ostream &operator<<(std::ostream &out, const std::unique_ptr<BinaryTree> &Bptr) {
+        if (Bptr) {
+            out << (*Bptr);
+        } else {
+            out << "(Leaf)";
         }
-        else
-        {
+        return out;
+    }
+
+   private:
+    void insertChild(const keyType k, const valType v, std::unique_ptr<BinaryTree> &c) {
+        if (c) {
+            c->insert(k, v);
+        } else {
             c = std::make_unique<BinaryTree>(k, v);
             c->parent = this;
         }
     }
 
-    valType lookupChild(const keyType k, std::unique_ptr<BinaryTree> &c)
-    {
-        if(c)
-        {
+    valType lookupChild(const keyType k, std::unique_ptr<BinaryTree> &c) {
+        if (c) {
             return c->lookup(k);
-        }
-        else
-        {
+        } else {
             throw std::runtime_error("Key value not found.");
         }
     }
