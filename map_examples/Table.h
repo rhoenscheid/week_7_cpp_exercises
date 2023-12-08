52e0bddb5775494ab681cd438136ba08d3023e8d
Milan Malfait <m.malfait@ucl.ac.uk>
2023-12-08 15:00:55 +0000

    Revert "Need to `#include <ostream>`"
    This reverts commit f9a8e82a09a757231dd412c5b283781a1685cd66.

--- map_examples/Table.h
+++ map_examples/Table.h
@@ -1,28 +1,38 @@
 #include <functional>
-#include <ostream>
 #include <stdexcept>
 #include <vector>
 
 using std::vector;
 
-template <typename keyType, typename valType>
-class Table {
-    struct kv_pair {
+template<typename keyType, typename valType>
+class Table
+{
+    struct kv_pair
+    {
         keyType key;
         valType val;
     };
 
-   public:
-    Table(const int N) : size(N) { table = vector<vector<kv_pair>>(size); }
+    public:
+    Table(const int N) : size(N)
+    {
+        table = vector< vector<kv_pair> >(size);
+    }
 
-    void insert(const keyType key, const valType val) {
+    void insert(const keyType key, const valType val)
+    {
         kv_pair kv{key, val};
         auto &row = table[index(key)];
-        if (row.empty()) {
+        if (row.empty())
+        {
             row.push_back(kv);
-        } else {
-            for (auto &row_entry : row) {
-                if (row_entry.key == key) {
+        }
+        else
+        {
+            for(auto &row_entry : row)
+            {
+                if(row_entry.key == key)
+                {
                     row_entry.val = val;
                     return;
                 }
@@ -31,23 +41,30 @@ class Table {
         }
     }
 
-    valType lookup(const keyType key) {
+    valType lookup(const keyType key)
+    {
         const auto &row = table[index(key)];
 
-        for (auto &row_entry : row) {
-            if (row_entry.key == key) {
+        for (auto &row_entry : row)
+        {
+            if (row_entry.key == key)
+            {
                 return row_entry.val;
             }
         }
         throw std::runtime_error("Key not found.");
     }
 
-    friend std::ostream &operator<<(std::ostream &out, const Table &h) {
-        for (int i = 0; i < h.size; i++) {
+    friend std::ostream &operator<<(std::ostream &out, const Table &h)
+    {
+        for(int i = 0; i < h.size; i++)
+        {
             const auto row = h.table[i];
-            if (!row.empty()) {
+            if(!row.empty())
+            {
                 out << i << ": ";
-                for (const auto entry : row) {
+                for(const auto entry : row)
+                {
                     out << "(" << entry.key << "," << entry.val << ") ";
                 }
                 out << std::endl;
@@ -56,10 +73,14 @@ class Table {
         return out;
     }
 
-   private:
-    int index(keyType key) { return hash(key) % size; }
+    private:
+
+    int index(keyType key)
+    {
+        return hash(key) % size;
+    }
 
     std::hash<keyType> hash;
     const int size;
-    vector<vector<kv_pair>> table;
+    vector< vector<kv_pair>> table;
 };
