eabecaa162cd074ddf76bf8506ddbc1037927250
Ruben Hoenscheid <ruben.honscheid.20@ucl.ac.uk>
2024-03-01 16:21:21 +0000

    Initial work on the matrix multiplication

---
+++ matrix_examples/build/CMakeFiles/MatrixOps.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatrixOps.dir/source/matrix_operations.cpp.o"
+  "CMakeFiles/MatrixOps.dir/source/matrix_operations.cpp.o.d"
+  "bin/MatrixOps"
+  "bin/MatrixOps.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MatrixOps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- matrix_examples/source/matrix_operations.cpp
+++ matrix_examples/source/matrix_operations.cpp
@@ -7,6 +7,16 @@
 using std::vector;
 typedef std::chrono::high_resolution_clock myClock;
 
+template<typename T, size_t N>
+void print_matrix(vector<vector<T>> mat){
+    for (int i = 0; i < N; i++){
+        std::cout << "Row " << i << std::endl;
+        for(int j = 0; j < N; j++){
+            std::cout << mat[i][j] << std::endl;
+        }
+    }
+}
+
 double t_seconds(std::chrono::time_point<myClock> t1, std::chrono::time_point<myClock> t2)
 {
     return (t2 - t1).count() / 1e9;
@@ -17,7 +27,42 @@ int main()
     std::mt19937_64 rng;
     std::uniform_real_distribution<double> dist(0,1);
 
-    const int N = 1000;
+    myClock *clock = new myClock();
+
+    //make three empty vectors
+
+
+    vector<vector<double>> A = {{1.0, 2.0, 3.0}, {1.0, 2.0, 3.0}, {1.0, 2.0, 3.0}};
+    vector<vector<double>> B = {{3.0, 2.0, 1.0}, {3.0, 2.0, 1.0}, {3.0, 2.0, 1.0}};
+    vector<vector<double>> C = {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}};
+
+
+
+
+    const int N = 3;
+
+    auto t1 = clock->now();
+
+    rowMajor_by_rowMajor(A, B, C, N);
+
+    auto t2 = clock->now();
+
+    //print_matrix<double, N>(C);
+
+    std::cout << "---- Time taken for rowMajor_by_rowMajor ----" << std::endl;
+    std::cout << t_seconds(t1, t2) << std::endl;
+
+    auto t3 = clock->now();
+
+    rowMajor_by_colMajor(A, B, C, N);
+
+    auto t4 = clock->now();
+
+    std::cout << "---- Time taken for rowMajor_by_colMajor ----" << std::endl;
+    std::cout << t_seconds(t3, t4) << std::endl;
+
+
+    //create a new matrix 
 
     // Some options for matrix representations!
     // double *X = new double[N*N];
@@ -25,5 +70,7 @@ int main()
     // double **X = new double *X[N];
     // vector<vector<double>> X(N);   
 
+
+    delete clock;
     return 0;
 }
